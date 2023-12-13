59683ed0d7c5f88f08e0a1fb570f8411f89a9644
remiFemenia <tjall@hotmail.fr>
2023-12-13 18:44:08 +0100

    Création de l'appel de la fonction createCardProject

--- Backend/script.js
+++ Backend/script.js
@@ -5,11 +5,11 @@ const project = works[0];
 function createCardProject (index) {
     for (let i = 0; i < works.lenght; i++) {
 
-        // Création de la balise img avec récupération de l'élément
+        // Création de la balise <img> avec récupération de l'élément
         const imageProject = document.createElement("img");
         imageProject.src = i.imageURL;
 
-        // Création de la balise fig avec récupération de l'élément
+        // Création de la balise <figcaption> avec récupération de l'élément
         const titleProject = document.createElement("figcaption");
         titleProject.innerText = numberProject.title;
 
@@ -18,11 +18,11 @@ function createCardProject (index) {
         const cardProject = document.createElement("figure");
         sectionProject.appendChild(cardProject);
 
-        //Rattachement des éléments image et titre à une balise figure
+        //Rattachement des balises <img> et <figcaption> à une balise <figure>
         cardProject.appendChild(imageProject);
         cardProject.appendChild(titleProject);
 
     }
 
 }
-
+createCardProject()
