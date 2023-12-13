f92b5745098cfbbd59fb05f2b694e6b7fb8ebacc
remiFemenia <tjall@hotmail.fr>
2023-12-13 20:04:01 +0100

    Finalisation de l'ajout des projets sur la page

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,29 +1,31 @@
-let reponse = await fetch("http://localhost:5678/api/works");
-console.log(reponse)
+fetch("http://localhost:5678/api/works")
+.then(response => response.json())
+.then(works => {
+console.log(works)
 
-const project = works[0];
+for (let i = 0; i < works.length; i++) {
+    createCardProject(works[i])
+}
 
-function createCardProject (index) {
-    for (let i = 0; i < works.lenght; i++) {
+}
+)
+
+function createCardProject (work) {
 
         // Création de la balise <img> avec récupération de l'élément
         const imageProject = document.createElement("img");
-        imageProject.src = i.imageURL;
+        imageProject.src = work.imageUrl;
 
         // Création de la balise <figcaption> avec récupération de l'élément
         const titleProject = document.createElement("figcaption");
-        titleProject.innerText = numberProject.title;
+        titleProject.innerText = work.title;
 
         // 
         const sectionProject = document.querySelector(".gallery");
         const cardProject = document.createElement("figure");
-        sectionProject.appendChild(cardProject);
-
         //Rattachement des balises <img> et <figcaption> à une balise <figure>
         cardProject.appendChild(imageProject);
         cardProject.appendChild(titleProject);
-
-    }
+        sectionProject.appendChild(cardProject);
 
 }
-createCardProject()
