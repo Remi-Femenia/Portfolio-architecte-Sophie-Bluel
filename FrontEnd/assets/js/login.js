930c82e7978269492f978b1c2cc0e16af76cef6b
remiFemenia <tjall@hotmail.fr>
2024-01-16 19:52:01 +0100

    Ajout de la fonction "login"

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,7 +1,14 @@
-const reponse = await fetch("http://localhost:5678/api/users/login"), {
+// Récupérer chaque champ et les values (.value)
+
+async function login (email, password) {
+const reponse = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: '{"email": "emailEntered"}'
-};
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({"email": email,
+            "password": password}
+    )  
+});
 
 let login = await reponse.json();
+
+}
