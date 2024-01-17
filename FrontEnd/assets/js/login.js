4eb1dad60d7023cf908a5037b61ef2b6e9a39fae
remiFemenia <tjall@hotmail.fr>
2024-01-17 18:45:46 +0100

    Ajout de l'id send-button

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,14 +1,18 @@
 // Récupérer chaque champ et les values (.value)
 
 async function login (email, password) {
-const reponse = await fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({"email": email,
-            "password": password}
-    )  
+    let sendBtn = document.getElementById("send-button");
+    sendBtn.addEventListener("click", function () {
+        console.log(sendBtn);
+
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({"email": email, "password": password}
+        )  
 });
 
 let login = await reponse.json();
 
 }
+)}
