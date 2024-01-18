27ab2d264258f4abcc4e646d9503d775b5bd4d0c
remiFemenia <tjall@hotmail.fr>
2024-01-18 15:23:14 +0100

    Récupération des champs de saisie

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,4 +1,12 @@
 // Récupérer chaque champ et les values (.value)
+let emailInput = document.getElementById("email");
+let email = emailInput.value
+let passwordInput = document.getElementById("password");
+
+console.log(email);
+
+
+
 
 async function login (email, password) {
     let sendBtn = document.getElementById("send-button");
