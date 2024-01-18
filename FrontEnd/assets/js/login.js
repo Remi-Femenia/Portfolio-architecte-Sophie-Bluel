2f2293b4042b851ea0185d5bd7996be22a10b50d
remiFemenia <tjall@hotmail.fr>
2024-01-18 20:04:51 +0100

    Mise en forme du fichier

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -28,10 +28,9 @@ async function login (email, password) {
     const reponse = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({"email": email, "password": password}
-        )
-        let loginResponse = await reponse.json();
-
+        body: JSON.stringify({"email": email, "password": password})
     });
+
+    let loginResponse = await reponse.json();
 
 }
