55897142d1f9859a121ba867f0f23f5db7e82be3
remiFemenia <tjall@hotmail.fr>
2024-01-25 12:19:06 +0100

    Ajout du message d'avertissement en cas d'erreur du mot de passe

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -9,10 +9,6 @@ form.addEventListener("submit", async (event) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    console.log(email);
-    console.log(password);
-
-
     if (email === "") {
         console.log("Le champ est vide");
     } else {
@@ -25,20 +21,39 @@ form.addEventListener("submit", async (event) => {
         console.log("Le mot de passe est saisi.")
     }
 
-    login(email, password) ;
+    login(email, password);
+
 })
 
 
 async function login (email, password) {
 
-    const reponse = await fetch("http://localhost:5678/api/users/login", {
+    const response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({"email": email, "password": password})
     });
-
-    let loginResponse = await reponse.json();
-
     
+    if (response.ok) {
+        let loginResponse = await response.json();
+
+        window.localStorage.setItem("id", loginResponse.userId);
+        window.localStorage.setItem("token", loginResponse.token);
+
+        window.location.href="index.html";
+    }
+    else {
+        const loginError = document.getElementById("login-error");
+        
+        const loginErrorMessage = 
+                <p>
+                L'e-mail et/ou le mot de passe saisis sont incorrects.
+                <br/>
+                Veuillez saisir un e-mail et un mot de passe valides.
+                </p>;
+
+        loginError.innerHTML = loginErrorMessage;
+        loginError.classList.add("login-error");
+    }
 
 }
