4a5aa6297474be271accca8b7b09c1871c304ac5
remiFemenia <tjall@hotmail.fr>
2024-01-29 18:40:30 +0100

    Ajout des fonctions d'affichage et fermeture de la modale

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -3,10 +3,44 @@
 openModifyModal.addEventListener("click", () => {
     dialog.showModal();
 })*/
+let modal = null;
 
-function openModal (e) {
-    
+const openModal = function (e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden', false);
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
 }
 
-const linkOpenModal = document.querySelector("open-modal");
-linkOpenModal.addEventListener("click", openModal);
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault();
+    target.style.display = none;
+    target.setAttribute('aria-hidden', 'true');
+    target.removeAttribute('aria-modal');
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+const linkOpenModal = document.querySelector("open-modal")(a => {
+    a.addEventListener("click", openModal);
+});
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape") {
+        closeModal(e);
+    }
+})
+
