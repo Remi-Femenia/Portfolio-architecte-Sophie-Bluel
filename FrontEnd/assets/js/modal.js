3bd025d5093583d54a9422cb78d4c4d015b5ed10
remiFemenia <tjall@hotmail.fr>
2024-02-06 19:57:01 +0100

    Affichage des images des travaux dans la modale

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -8,25 +8,31 @@ openModifyModal.addEventListener("click", () => {
 let apiWorks = await fetch("http://localhost:5678/api/works");
 let worksList = await apiWorks.json();
 
+
+
 ////// Création des travaux dans la fenêtre modale 1 //////
 async function createModalWorks (work) {
 
     // Images des travaux
-    const imgWorks = worksList.imageUrl;
+    const imgWorks = work.imageUrl;
     const modalWorkImage = document.createElement("img");
     modalWorkImage.src = imgWorks;
     modalWorkImage.classList.add("modal1-img-work");
 
     // Icône de suppression des travaux
     const modalDeleteIcon = document.createElement("i");
-    modalDeleteIcon.classList.add("fa-solid fa-trash-can modal1-delete-icon");
+    modalDeleteIcon.classList.add("fa-solid", "fa-trash-can", "modal1-delete-icon");
 
     // Gestion des conteneurs //
     const modalWorksGalleryContainer = document.getElementById("modal1-works-gallery");
     const modalWorkElement = document.createElement("div");
 
-    modalWorkElement.appendChild("modalWorkImage"); 
-    modalWorkElement.appendChild("modalDeleteIcon");
+    modalWorkElement.appendChild(modalWorkImage); 
+    modalWorkElement.appendChild(modalDeleteIcon);
+    modalWorksGalleryContainer.appendChild(modalWorkElement);
+
+    // Ajouter class à la div conteneur de chaque élément
+    // Grâce aux DataSet identifier les icônes correspondants aux travaux pour les supprimer
 
 }
 
@@ -39,6 +45,11 @@ const openModal = function (e, a) {
     const target = document.querySelector(a.getAttribute('href'));
     target.style.display = "flex";
     target.removeAttribute('aria-hidden');
+
+    for (let i = 0; i < worksList.length; i++) {
+        
+        createModalWorks(worksList[i]); 
+    }
 }
 
 const closeModal = function (e, a) {
