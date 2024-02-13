e3b20938a14a8f8feeba96b90df33f9d57d2ac37
remiFemenia <tjall@hotmail.fr>
2024-02-13 18:57:01 +0100

    Fonction de suppression des travaux

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -8,6 +8,40 @@ openModifyModal.addEventListener("click", () => {
 let apiWorks = await fetch("http://localhost:5678/api/works");
 let worksList = await apiWorks.json();
 
+let modal = null;
+const focusableSelector = "button, a, input, textarea";
+let focusables = [];
+
+// Fonction d'ouverture de la modale 1
+const openModal = function (e, a) {
+    e.preventDefault();
+    modal = document.querySelector(a.getAttribute('href'));
+    modal.style.display = "flex";
+    modal.removeAttribute('aria-hidden');
+
+    for (let i = 0; i < worksList.length; i++) {
+        
+        createModalWorks(worksList[i]);
+        
+    }
+}
+
+/////Fonction de suppression des projets
+async function deleteWork (id) {
+    
+    const apiWorksDelete = await fetch("http://localhost:5678/api/works/{id}", {
+        method: "DELETE",
+        headers: {},
+        body: JSON.stringify({"id": workId})
+        },
+    )}
+
+    for (let i = 0; i < worksList.length; i++) {
+        const deleteBtn = document.querySelector("modal1-delete-icon");
+        deleteBtn.addEventListener("click", (id){
+            vdfgdg
+        })
+    }
 
 
 ////// Création des travaux dans la fenêtre modale 1 //////
@@ -33,23 +67,14 @@ async function createModalWorks (work) {
     modalWorksGalleryContainer.appendChild(modalWorkElement);
 
     // Grâce aux DataSet identifier les icônes correspondants aux travaux pour les supprimer
-
 }
 
 
-let modal = null;
 
 
-const openModal = function (e, a) {
-    e.preventDefault();
-    const target = document.querySelector(a.getAttribute('href'));
-    target.style.display = "flex";
-    target.removeAttribute('aria-hidden');
+const openModal2 = function (e,a) {
 
-    for (let i = 0; i < worksList.length; i++) {
-        
-        createModalWorks(worksList[i]); 
-    }
+
 }
 
 const closeModal = function (e, a) {
@@ -79,9 +104,14 @@ document.querySelectorAll(".js-modal-stop").forEach(a => {
     a.addEventListener("click", stopPropagation);
 })
 
-// Fermer la modale avec le bouton Esc
+// Gestion des touches clavier pour la navigation
 window.addEventListener('keydown', function (e) {
+    //Gestion de la touche Echap dans la navigation de la modale
     if (e.key === "Escape" || e.key === "Esc") {
         closeModal(e);
+    }
+    // Gestion des Tab dans la navigation dans la modale
+    if (e.key === "Tab" && modal !== null) {
+        focusInModal(e)
     }
 })
