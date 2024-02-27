5a319f7c7015fd3b23a8afd6bf043cec77d1e8e8
remiFemenia <tjall@hotmail.fr>
2024-02-27 20:01:49 +0100

    Ouverture et fermture de modales

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -82,3 +82,149 @@ function filterProject (categorie) {
     })
 
 }
+
+// Importation des images des travaux de l'API
+let apiWorks = await fetch("http://localhost:5678/api/works");
+let worksList = await apiWorks.json();
+
+let modal = null;
+const focusableSelector = "button, a, input, textarea";
+let focusables = [];
+
+
+// ID de l'utilisateur
+const userId = window.localStorage.getItem("userId");
+
+// Fonction d'ouverture de la modale 1
+const openModal = function (event, element) {
+
+    event.preventDefault();
+    modal = document.querySelector(element.dataset.open);
+    modal.style.display = "flex";
+    modal.removeAttribute('aria-hidden');
+    document.getElementById("modal1-works-gallery").innerHTML = "";
+
+    for (let i = 0; i < worksList.length; i++) {
+        createModalWorks(worksList[i]);
+    }
+
+    deleteEvent();
+
+}
+
+
+/////Fonction de suppression des projets
+async function deleteWork (id) {
+
+    const apiWorksDelete = await fetch("http://localhost:5678/api/works/" + id, {
+        
+        method: "DELETE",
+        body: JSON.stringify({"userId": userId})
+
+    },
+
+)}
+        
+
+
+////// Création des images des travaux dans la fenêtre modale 1 //////
+async function createModalWorks (work) {
+
+    // Images des travaux
+    const imgWorks = work.imageUrl;
+    const modalWorkImage = document.createElement("img");
+    modalWorkImage.src = imgWorks;
+    modalWorkImage.classList.add("modal1-img-work");
+
+    // Icône de suppression des travaux
+    const modalDeleteIcon = document.createElement("i");
+    modalDeleteIcon.classList.add("fa-solid", "fa-trash-can", "fa-xs", "modal1-delete-icon");
+    modalDeleteIcon.dataset.id = work.id;
+
+    // Gestion des conteneurs //
+    const modalWorksGalleryContainer = document.getElementById("modal1-works-gallery");
+    const modalWorkElement = document.createElement("div");
+    modalWorkElement.classList.add("modal1-work-element")
+
+    modalWorkElement.appendChild(modalWorkImage); 
+    modalWorkElement.appendChild(modalDeleteIcon);
+    modalWorksGalleryContainer.appendChild(modalWorkElement);
+
+}
+
+function deleteEvent () {
+
+    const deleteBtn = document.querySelectorAll(".modal1-delete-icon");
+
+    for (let i = 0; i < deleteBtn.length; i++) {
+        deleteBtn[i].addEventListener("click", () => {
+        deleteWork (deleteBtn[i].dataset.id);
+         })
+    }
+
+    const modalWorksGallery = document.getElementById("modal1-works-gallery");
+    modalWorksGallery.innerHTML = "";
+
+    for (let i = 0; i < worksList.length; i++) { 
+
+        createModalWorks(worksList[i]);
+
+    }
+
+    // Rechargement de la gallery du portfolio
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    for (let i = 0; i < worksList.length; i++) {  
+        createCardProject(worksList[i]);
+    }
+}
+
+///// Ouverture de la modale 2 /////
+const openModal2 = function (event, element) {
+
+
+}
+
+
+// Fermeture de la modale 1
+const closeModal = function (event, element) {
+    event.preventDefault();
+    modal = document.querySelector(element.dataset.close);
+    console.log(element.dataset.close);
+    modal.style.display ="none";
+}
+
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+//// Élément d'ouverture de la modale
+document.querySelectorAll('.js-modal').forEach(element => {
+    element.addEventListener("click", event => openModal (event, element));
+});
+
+document.querySelectorAll('.js-modal-close').forEach(a => {
+    a.addEventListener("click", e => closeModal (e, a));
+});
+
+document.querySelectorAll(".modal").forEach(a => {
+    a.addEventListener("click", e => closeModal (e, a));
+})
+
+document.querySelectorAll(".js-modal-stop").forEach(a => {
+    a.addEventListener("click", stopPropagation);
+})
+
+// Gestion des touches clavier pour la navigation
+window.addEventListener('keydown', function (e) {
+    //Gestion de la touche Echap dans la navigation de la modale
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    // Gestion des Tab dans la navigation dans la modale
+    if (e.key === "Tab" && modal !== null) {
+        focusInModal(e)
+    }
+})
